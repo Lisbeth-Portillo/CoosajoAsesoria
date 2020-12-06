4887fcd6797993babc4ad3df630590c0b223aee3
Byron Cruz <byronalexandergarcia@gmail.com>
2020-12-05 22:52:11 -0600

    funciones para el correcto manejo de centavos en los formularios (functions.js)

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,15 +1,39 @@
+/*************************LINEA BASE ASOCIADO/*************************/
 
-
-$("input[name=total_empleados]").click(function(){
-    alert("hola mundo");
+//esta funcion hara que los campos acepten unicamente dos decimales para el manejo correcto de centavos
+$(".solodosdecimales").change(function () {
+    let numero = Number($(this).val())
+    let conDecimal = numero.toFixed(2)
+    $(this).val(conDecimal)
 })
 
+
+function ultimostresanios() {
+    var fecha = new Date();
+    var anio = fecha.getFullYear();
+    $(".ultimostresanios input").each(function () {
+        $(this).val(anio)
+        anio--
+    })
+}
+ultimostresanios();
+/*********************************************************************/
+
+
+
+
+
+
+/* $("input[name=total_empleados]").click(function(){
+    alert("hola mundo");
+}) */
+
 /*************************CARTA ASOCIADO/*************************/
-$(".carta .firma #texto_firma").keyup(function(){
+/* $(".carta .firma #texto_firma").keyup(function(){
     let input = $(this)
     let firma = input.text()
     $(".almacenador_texto_firma").val(firma)
-})
+}) */
 /*****************************************************************/
 
 
